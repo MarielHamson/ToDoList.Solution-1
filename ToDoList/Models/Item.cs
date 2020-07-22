7e66381502cf41b80da17f1c9d2fb8ee6b518d46
James Henager <jameschenager@gmail.com>
2020-07-22 10:56:41 -0700

    add id property to item and passing test
    Co-authored-by: Joseph Pearce <joseph.pearce05@gmail.com>
    Co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -7,12 +7,14 @@ namespace ToDoList.Models
   {
 
     public string Description { get; set; }
+    public int Id { get; }
     private static List<Item> _instances = new List<Item> {};
 
     public Item(string description)
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
       
     }
 
